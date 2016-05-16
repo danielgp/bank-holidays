202baa5e1f7b8bf4d5a3ffda0a8bdf9f8b671024
Daniel Popiniuc <danielpopiniuc@gmail.com>
2016-05-16 19:03:01 +0300

    adjusted the test to latest PHPUnit version and to DateTime OOP class

--- tests/php-unit/RomanianTest.php
+++ tests/php-unit/RomanianTest.php
@@ -30,114 +30,107 @@ class RomanianTest extends \PHPUnit_Framework_TestCase
 
     use \danielgp\bank_holidays\Romanian;
 
-    public function testHolidaysEaster2015CatholicEasterFirstDay()
-    {
+    public function testHolidaysFixedButWorkingIncluded() {
         // Arrange
-        $a = $this->setHolidays(strtotime('2015-04-01'), true);
+        $a = $this->setHolidays(new \DateTime('2016-04-01'), true, true);
+        // Assert
+        $this->assertContains(strtotime('2016-02-19'), $a);
+    }
+
+    public function testHolidaysEaster2015CatholicEasterFirstDay() {
+        // Arrange
+        $a = $this->setHolidays(new \DateTime('2015-04-01'), true);
         // Assert
         $this->assertContains(strtotime('2015-04-05'), $a);
     }
 
-    public function testHolidaysEaster2015CatholicEasterSecondDay()
-    {
+    public function testHolidaysEaster2015CatholicEasterSecondDay() {
         // Arrange
-        $a = $this->setHolidays(strtotime('2015-04-01'), true);
+        $a = $this->setHolidays(new \DateTime('2015-04-01'), true);
         // Assert
         $this->assertContains(strtotime('2015-04-06'), $a);
     }
 
-    public function testHolidaysEaster2015FirstDayOfYear()
-    {
+    public function testHolidaysEaster2015FirstDayOfYear() {
         // Arrange
-        $a = $this->setHolidays(strtotime('2015-12-01'));
+        $a = $this->setHolidays(new \DateTime('2015-12-01'));
         // Assert
         $this->assertContains(strtotime('2015-01-01'), $a);
     }
 
-    public function testHolidaysEaster2015LastDayOfYear()
-    {
+    public function testHolidaysEaster2015LastDayOfYear() {
         // Arrange
-        $a = $this->setHolidays(strtotime('2015-12-01'));
+        $a = $this->setHolidays(new \DateTime('2015-12-01'));
         // Assert
         $this->assertNotContains(strtotime('2015-12-31'), $a);
     }
 
-    public function testHolidaysEaster2015OrthodoxEasterFirstDay()
-    {
+    public function testHolidaysEaster2015OrthodoxEasterFirstDay() {
         // Arrange
-        $a = $this->setHolidays(strtotime('2015-04-01'), true);
+        $a = $this->setHolidays(new \DateTime('2015-04-01'), true);
         // Assert
         $this->assertContains(strtotime('2015-04-12'), $a);
     }
 
-    public function testHolidaysEaster2015OrthodoxEasterSecondDay()
-    {
+    public function testHolidaysEaster2015OrthodoxEasterSecondDay() {
         // Arrange
-        $a = $this->setHolidays(strtotime('2015-04-01'), true);
+        $a = $this->setHolidays(new \DateTime('2015-04-01'), true);
         // Assert
         $this->assertContains(strtotime('2015-04-13'), $a);
     }
 
-    public function testHolidaysInMonthForMonthWithCatholicEaster()
-    {
+    public function testHolidaysInMonthForMonthWithCatholicEaster() {
         // Arrange
-        $a = $this->setHolidaysInMonth(strtotime('2015-04-01'), true);
+        $a = $this->setHolidaysInMonth(new \DateTime('2015-04-01'), true);
         // Assert
         $this->assertEquals(4, $a);
     }
 
-    public function testHolidaysInMonthForMonthWithoutCatholicEaster()
-    {
+    public function testHolidaysInMonthForMonthWithoutCatholicEaster() {
         // Arrange
-        $a = $this->setHolidaysInMonth(strtotime('2015-04-01'), false);
+        $a = $this->setHolidaysInMonth(new \DateTime('2015-04-01'), false);
         // Assert
         $this->assertEquals(2, $a);
     }
 
-    public function testWorkingDaysInMonthForMonthOf19Days()
-    {
+    public function testWorkingDaysInMonthForMonthOf19Days() {
         // Arrange
-        $a = $this->setWorkingDaysInMonth(strtotime('2001-12-01'));
+        $a = $this->setWorkingDaysInMonth(new \DateTime('2001-12-01'));
         // Assert
         $this->assertEquals(19, $a);
     }
 
-    public function testWorkingDaysInMonthForMonthOf20Days()
-    {
+    public function testWorkingDaysInMonthForMonthOf20Days() {
         // Arrange
-        $a = $this->setWorkingDaysInMonth(strtotime('2001-02-01'));
+        $a = $this->setWorkingDaysInMonth(new \DateTime('2001-02-01'));
         // Assert
         $this->assertEquals(20, $a);
     }
 
-    public function testWorkingDaysInMonthForMonthOf20DaysWithCatholicEaster()
-    {
+    public function testWorkingDaysInMonthForMonthOf20DaysWithCatholicEaster() {
         // Arrange
-        $a = $this->setWorkingDaysInMonth(strtotime('2015-04-01'), true);
+        $a = $this->setWorkingDaysInMonth(new \DateTime('2015-04-01'), true);
         // Assert
         $this->assertEquals(20, $a);
     }
 
-    public function testWorkingDaysInMonthForMonthOf21Days()
-    {
+    public function testWorkingDaysInMonthForMonthOf21Days() {
         // Arrange
-        $a = $this->setWorkingDaysInMonth(strtotime('2006-01-01'));
+        $a = $this->setWorkingDaysInMonth(new \DateTime('2006-01-01'));
         // Assert
         $this->assertEquals(21, $a);
     }
 
-    public function testWorkingDaysInMonthForMonthOf22Days()
-    {
+    public function testWorkingDaysInMonthForMonthOf22Days() {
         // Arrange
-        $a = $this->setWorkingDaysInMonth(strtotime('2016-09-01'), false);
+        $a = $this->setWorkingDaysInMonth(new \DateTime('2016-09-01'), false);
         // Assert
         $this->assertEquals(22, $a);
     }
 
-    public function testWorkingDaysInMonthForMonthOf23Days()
-    {
+    public function testWorkingDaysInMonthForMonthOf23Days() {
         // Arrange
-        $a = $this->setWorkingDaysInMonth(strtotime('2015-07-01'));
+        $a = $this->setWorkingDaysInMonth(new \DateTime('2015-07-01'));
         // Assert
         $this->assertEquals(23, $a);
     }
